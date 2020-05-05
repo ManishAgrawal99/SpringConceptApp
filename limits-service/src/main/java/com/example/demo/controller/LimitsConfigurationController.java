e36822d7259cc57674e6380a14fb3b7b043ae87e
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-05-05 13:14:15 +0530

    Fetching Limits from properties

--- limits-service/src/main/java/com/example/demo/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/demo/controller/LimitsConfigurationController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.config.Configuration;
 import com.example.demo.model.LimitsConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsFromConfiguration() {
-		return new LimitsConfiguration(1000, 1);
+		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
