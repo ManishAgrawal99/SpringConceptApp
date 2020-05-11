f681d4dabf6606c363b86c3b3db9c509a23940e5
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-05-11 14:28:32 +0530

    Using Ribbon Load Balancing

--- currency-conversion-service/src/main/java/com/example/demo/feignClient/CurrencyExchangeFeign.java
+++ currency-conversion-service/src/main/java/com/example/demo/feignClient/CurrencyExchangeFeign.java
@@ -1,14 +1,16 @@
 package com.example.demo.feignClient;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.model.ExchangeValue;
 
-@FeignClient(name="currency-exchange-service", url = "http://localhost:8000/currency-exchange/")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeFeign {
 	
-	@GetMapping("/from/{from}/to/{to}")
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
