5a05d79e88a7d4c40115fe1201644d6a0733a33c
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-05-11 15:51:09 +0530

    Added Eureka Discovery Server

--- currency-conversion-service/src/main/java/com/example/demo/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/demo/CurrencyConversionServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
-	
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
